0d0f0fe38ee426bc0085bb9b2d4df1cea80ef9f5
I Putu Cahya Adi Ganesha <iputucaganesha@gmail.com>
2022-06-11 16:39:13 +0700

    Revamp README
    Added a LOT of additional info

--- build_image.sh
+++ build_image.sh
@@ -1,5 +1,5 @@
 pack build \
   --builder gcr.io/buildpacks/builder:v1 \
   --env GOOGLE_FUNCTION_SIGNATURE_TYPE=http \
-  --env GOOGLE_FUNCTION_TARGET=helloWorld \
+  --env GOOGLE_FUNCTION_TARGET=getSubdistrictInfo \
   get_subdistrict_stat
